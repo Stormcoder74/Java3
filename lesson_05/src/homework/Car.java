eb269aa1f39527f35de8c02c14d65a33c2efd612
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-07-16 22:19:21 +1100

    дз к 5 уроку (с классом Synchronizer)

--- lesson_05/src/homework/Car.java
+++ lesson_05/src/homework/Car.java
@@ -1,5 +1,7 @@
 package homework;
 
+import java.util.concurrent.BrokenBarrierException;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
 
@@ -10,12 +12,14 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private Synchronizer synch;
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, Synchronizer synch) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.synch = synch;
     }
 
     public String getName() {
@@ -32,11 +36,19 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            synch.ready.countDown();
+            synch.start.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        race.winDetermination(this);
+        try {
+            synch.finish.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
         }
     }
 }
--- lesson_05/src/homework/MainClass.java
+++ lesson_05/src/homework/MainClass.java
@@ -1,19 +1,35 @@
 package homework;
 
+import java.util.concurrent.BrokenBarrierException;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
+        Synchronizer synch = new Synchronizer(CARS_COUNT, 1, CARS_COUNT + 1);
+
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), synch);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        try {
+            synch.ready.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        synch.start.countDown();
+        try {
+            synch.finish.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }

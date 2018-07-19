7053ab4592ee6b87c56a378535522d3a46ac1b9a
Teryaev Sergey <Stormcoder74@users.noreply.github.com>
2018-07-20 00:08:29 +1100

    работает на 4 вариантах входных данных

--- lesson_06/src/test/java/homework/Task2Test.java
+++ lesson_06/src/test/java/homework/Task2Test.java
@@ -1,20 +1,39 @@
 package homework;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class Task2Test {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{3, 5, 1, 3, 4, 7, 4, 6},
+                        new int[]{3, 5, 6, 4, 7, 8, 5, 6}},
+                {new int[]{4, 4, 4, 4, 4, 4, 4, 1},
+                        new int[]{3, 5, 6, 14, 7, 8, 5, 6}},
+                {new int[]{4, 1, 1, 1, 1, 1, 1, 1},
+                        new int[]{1, 1, 1, 1, 1, 1, 1, 1}},
+                {new int[]{3 - 2, 5, 0, 3, 0, 7, 8, 2 * 2},
+                        new int[]{4, 4, 4, 4, 4, 4, 4, 4}}
+        });
+    }
 
     private Task2 task2;
     private final int[] existArr;
     private final int[] absentArr;
 
-    public Task2Test() {
-        this.existArr = new int[]{3, 5, 1, 3, 4, 7, 4, 6};
-        this.absentArr = new int[]{3, 5, 6, 4, 7, 8, 5, 6};
+    public Task2Test(int[] existArr, int[] absentArr) {
+        this.existArr = existArr;
+        this.absentArr = absentArr;
     }
 
     @Before
